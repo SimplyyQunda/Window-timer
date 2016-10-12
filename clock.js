279dae67907861deee536eac682e770347a8e18e
DaQunda Magwood <dmagwood13@gmail.com>
2016-10-12 08:41:37 -0400

    after a battle, its all complete

--- clock.js
+++ clock.js
@@ -1,38 +1,32 @@
 
-
-
-
-// m= checkTime(m)
-// s= checkTime(s)
-// h= checkTime(h)
-
  function clocktime () {
-	var today = new Date()
+	var today = new Date();
 	var hour = today.getHours();
 	var minutes = today.getMinutes();
 	var seconds = today.getSeconds();
+
 	var showhour = document.getElementById("hours");
 	if (hour >= 10) {
 		showhour.innerHTML = hour + ":"
 	} else {
-		showhour = "0" + hour + ":"
+		showhour.innerHTML = "0" + hour + ":"
 	}
 
 
 var showmins = document.getElementById("minutes");
 	if (minutes >= 10) {
-	showmins. innerHTML = minutes + ":"
+	showmins.innerHTML = minutes + ":"
 	} else {
-		showmins = "0" + minutes + ":"
+		showmins.innerHTML = "0" + minutes + ":"
 	}
 
 
 
 var showsec = document.getElementById("seconds");
 if (seconds >= 10) {
-	showsec. innerHTML = seconds 
+	showsec.innerHTML = seconds 
 	} else {
-		showsec = "0" + seconds 
+		showsec.innerHTML = "0" + seconds 
 	}
 };
 
@@ -40,24 +34,41 @@ setInterval (clocktime,1000);
 
 
 
+ function colorpop() {
+ 	var today = new Date();
+	var hour = today.getHours();
+	var minutes = today.getMinutes();
+	var seconds = today.getSeconds();
+
+	// var bgcolor =today.getBackgroundcolor(#f5b405);
+ 	
+ 	var showhex = document.getElementById("hexcode");
+ 	showhex.innerHTML = "<p>#"
+ 	+ hour + minutes + seconds  + "</p>"
+
+
+
+ };
+setInterval(colorpop,1000);
 
 
 
 
+var hexCodeNumber = 123456
+
+function hexCodeChange () {
+	hexCodeNumber = hexCodeNumber + 20;
+};
+
+function hexColorBackground () {
+	var hexBlock = document.getElementById("hexCode");
+	hexBlock.innerHTML= "<div style=\"background-color: " + hexCodeNumber + ";\"> #" + hexCodeNumber;
+	// hexBlock.style.backgroundColor = ("#" + hexCodeNumber);
+};
+
+setInterval (clocktime,1000);
+setInterval (hexColorBackground,500)
+setInterval (hexCodeChange,500)
 
 
-
-
-
-
-
-
-
-
-// 	// console.log(new Date());
-// var timeclock = document.getElementById("timeclock")
-// new Date(hour[, minutes[, seconds[, milliseconds]]]);
-// }
-
-// function 
 
